4e4bdd327612995ae091502752bdfdc917f57c2d
jonathans199 <jso1919@gmail.com>
2023-11-08 12:28:18 -0500

    first commit pulling from local api

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Button, Image, ScrollView, SafeAreaView } from 'react-native'
+import PostCard from './components/PostCard'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [blogPosts, setBlogPosts] = useState([])
+	// useEffect is being called on different lifecycle of the component
+	// 1. When component Mounts
+	// 2. when component unMounts
+
+	// How to get a component to re-render
+	// 1. change state variable
+	// 2. change props
+
+	useEffect(() => {
+		fetch('http://192.168.10.207:8080')
+			.then(res => res.json())
+			.then(data => setBlogPosts(data))
+			.catch(err => console.error(err))
+	}, [])
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<StatusBar style='auto' />
+			<View>
+				<ScrollView>
+					{blogPosts.map((singlePost, index) => {
+						return <PostCard singlePost={singlePost} index={index} key={singlePost._id} />
+					})}
+				</ScrollView>
+			</View>
+		</SafeAreaView>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		// display: 'flex',
+		// height: '100%',
+		// width: '100%',
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
